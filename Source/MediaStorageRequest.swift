f4a80ad54697397c9cdfa75bb8919b531502283f
hishida <hiroshi.ishida@jrits.ricoh.co.jp>
2016-07-14 11:43:21 +0900

    NECO-161 enabled to search.

--- Source/MediaStorageRequest.swift
+++ Source/MediaStorageRequest.swift
@@ -6,7 +6,7 @@
 import Foundation
 
 class MediaStorageRequest {
-    static func get(url url: String, queryParams: Dictionary<String, String>, header: Dictionary<String, String>, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) {
+    static func get(url url: String, queryParams: Dictionary<String, AnyObject>, header: Dictionary<String, String>, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) {
         var requestUrl = url
         if queryParams.count > 0 {
             requestUrl += "?" + joinParameters(params: queryParams)
@@ -15,7 +15,6 @@ class MediaStorageRequest {
             url: requestUrl,
             method: "GET",
             header: header,
-            params: [String: String](),
             completionHandler: completionHandler
         )
     }
@@ -25,7 +24,17 @@ class MediaStorageRequest {
             url: url,
             method: "POST",
             header: header,
-            params: params,
+            body: joinParameters(params: params).dataUsingEncoding(NSUTF8StringEncoding),
+            completionHandler: completionHandler
+        )
+    }
+    
+    static func post(url url: String, header: Dictionary<String, String>, body: NSData? = nil, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) {
+        sendRequest(
+            url: url,
+            method: "POST",
+            header: header,
+            body: body,
             completionHandler: completionHandler
         )
     }
@@ -54,13 +63,12 @@ class MediaStorageRequest {
             url: url,
             method: "DELETE",
             header: header,
-            params: [String: String](),
             completionHandler: completionHandler
         )
     }
     
-    static func sendRequest(url url: String, method: String, header: Dictionary<String, String>, params: Dictionary<String, String>, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) {
-        let request = generateRequest(url: url, method: method, header: header, params: params)
+    static func sendRequest(url url: String, method: String, header: Dictionary<String, String>, body: NSData? = nil, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) {
+        let request = generateRequest(url: url, method: method, header: header, body: body)
         let config = NSURLSessionConfiguration.defaultSessionConfiguration()
         let session = NSURLSession(configuration: config)
         let task = session.dataTaskWithRequest(request, completionHandler: completionHandler)
@@ -68,7 +76,7 @@ class MediaStorageRequest {
     }
     
     static func sendRequestToUpload(url url: String, method: String, header: Dictionary<String, String>, data: NSData, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) {
-        let request = generateRequest(url: url, method: method, header: header, params: [String: String]())
+        let request = generateRequest(url: url, method: method, header: header)
         let config = NSURLSessionConfiguration.defaultSessionConfiguration()
         let session = NSURLSession(configuration: config)
         let task = session.uploadTaskWithRequest(request, fromData: data, completionHandler: completionHandler)
@@ -76,24 +84,24 @@ class MediaStorageRequest {
     }
     
     static func sendRequestToDownload(url url: String, method: String, header: Dictionary<String, String>, completionHandler: (NSURL?, NSURLResponse?, NSError?) -> Void) {
-        let request = generateRequest(url: url, method: method, header: header, params: [String: String]())
+        let request = generateRequest(url: url, method: method, header: header)
         let config = NSURLSessionConfiguration.defaultSessionConfiguration()
         let session = NSURLSession(configuration: config)
         let task = session.downloadTaskWithRequest(request, completionHandler: completionHandler)
         task.resume()
     }
     
-    static func generateRequest(url url: String, method: String, header: Dictionary<String, String>, params: Dictionary<String, String>) -> NSMutableURLRequest {
+    static func generateRequest(url url: String, method: String, header: Dictionary<String, String>, body: NSData? = nil) -> NSMutableURLRequest {
         let request = NSMutableURLRequest(URL: NSURL(string: url)!)
         request.HTTPMethod = method
         for (key, value) in header {
             request.setValue(value, forHTTPHeaderField: key)
         }
-        request.HTTPBody = joinParameters(params: params).dataUsingEncoding(NSUTF8StringEncoding)
+        request.HTTPBody = body
         return request
     }
     
-    static func joinParameters(params params: Dictionary<String, String>) -> String {
+    static func joinParameters(params params: Dictionary<String, AnyObject>) -> String {
         return params.map({(key, value) in
             return "\(key)=\(value)"
         }).joinWithSeparator("&")
