6f3bb322f8ab327f3c96bd146c4c8ad71315ec0c
Tomoko Sohda <tomoko.sohda@jrits.ricoh.co.jp>
2016-07-15 08:39:35 +0900

    2nd Reviewed Point

--- Source/MediaStorage.swift
+++ Source/MediaStorage.swift
@@ -243,14 +243,14 @@ public class MediaStorage {
     public func addMeta(mediaId mediaId: String, userMeta: Dictionary<String, String>, completionHandler: MediaStorageError -> Void) {
         if accessToken == nil {
             completionHandler(
-                MediaStorageError(statusCode: nil, message: "userMeta is empty: nothing to request.")
+                MediaStorageError(statusCode: nil, message: "wrong usage: use the connect method to get an access token.")
             )
             return
         }
         
         if userMeta.isEmpty {
             completionHandler(
-                MediaStorageError(statusCode: nil, message: "empty userMeta was taken: nothing to request.")
+                MediaStorageError(statusCode: nil, message: "userMeta is empty: nothing to request.")
             )
         }
         
